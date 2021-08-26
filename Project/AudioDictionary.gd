4706be1d2468316c1cac2e82f3d70feae234fa19
Priyanka Gunjal <prigun@avalanchestudios.se>
2021-08-27 01:17:15 +0200

    Adding AudioDictionary.

---
+++ Project/AudioDictionary.gd
@@ -0,0 +1,24 @@
+extends Node2D
+var AudioStreams = {
+	"BACKGROUND" : AudioStream,
+	"WALK" : AudioStream,
+	"JUMP" : AudioStream,
+	"ATTACK": AudioStream,
+	"BLOCK": AudioStream,
+	"HURT": AudioStream
+}
+
+func _ready():
+	AudioStreams.BACKGROUND = load("res://res/Audio/test.ogg")
+	AudioStreams.WALK = load("res://res/Audio/test.ogg")
+	AudioStreams.JUMP = load("res://res/Audio/test.ogg")
+	AudioStreams.ATTACK = load("res://res/Audio/test.ogg")
+	AudioStreams.BLOCK = load("res://res/Audio/test.ogg"
+	AudioStreams.HURT = load("res://res/Audio/test.ogg")
+	pass
+	
+
+	
+	   
+	
+
