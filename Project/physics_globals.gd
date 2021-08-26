1433b890a303401cd22d136f7163d2e2c544bf64
Puru Kaushik <kaushik.swapnil5@gmail.com>
2021-08-26 15:38:31 +0200

    Adding mappings for jump, adding player.gd, physics_globals.gd

---
+++ Project/physics_globals.gd
@@ -0,0 +1,10 @@
+const UP = Vector2(0, -1)
+const DOWN = Vector2(0, 1)
+const LEFT = Vector2(0, -1)
+const RIGHT = Vector2(0, 1)
+
+const GRAVITY_FACTOR = 9.8
+const GRAVITY_VEC = Vector2(0, GRAVITY_FACTOR)
+
+const MAX_SPEED = 20.0
+const MAX_FALL_SPEED = 10.0
