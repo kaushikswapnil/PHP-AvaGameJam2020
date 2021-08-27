fc2e2e5675b74b92fd324e42d5708f364df9f4ef
Priyanka Gunjal <prigun@avalanchestudios.se>
2021-08-27 13:27:48 +0200

    Adding Audios.

--- Project/AudioDictionary.gd
+++ Project/AudioDictionary.gd
@@ -3,18 +3,18 @@ var AudioStreams = {
 	"BACKGROUND" : AudioStream,
 	"WALK" : AudioStream,
 	"JUMP" : AudioStream,
-	"ATTACK": AudioStream,
-	"BLOCK": AudioStream,
+	"SWORDCLASH": AudioStream,
+	"SWORDSLASH": AudioStream,
 	"HURT": AudioStream
 }
 
 func _ready():
-	AudioStreams.BACKGROUND = load("res://res/Audio/test.ogg")
-	AudioStreams.WALK = load("res://res/Audio/test.ogg")
-	AudioStreams.JUMP = load("res://res/Audio/test.ogg")
-	AudioStreams.ATTACK = load("res://res/Audio/test.ogg")
-	AudioStreams.BLOCK = load("res://res/Audio/test.ogg")
-	AudioStreams.HURT = load("res://res/Audio/test.ogg")
+	AudioStreams.BACKGROUND = load("res://res/Audio/BattleBackgroundMusic.ogg")
+	AudioStreams.WALK = load("res://res/Audio/steps.ogg")
+	AudioStreams.JUMP = load("res://res/Audio/jumpland.ogg")
+	AudioStreams.SWORDCLAN = load("res://res/Audio/sword_clash.ogg")
+	AudioStreams.SWORDSLASH = load("res://res/Audio/sword_slash.ogg")
+	AudioStreams.HURT = load("res://res/Audio/hurt.ogg")
 	
 
 	
