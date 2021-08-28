5722beaf9be988ac1fdaf411e50c2c08cd61eef8
Puru Kaushik <kaushik.swapnil5@gmail.com>
2021-08-28 21:39:15 +0200

    Adding random portal

--- Project/LevelBackground.gd
+++ Project/LevelBackground.gd
@@ -3,6 +3,8 @@ extends Sprite
 onready var m_Portals = []
 onready var m_PortalDirections = []
 
+onready var m_TotalDelta = 0.0
+
 func _ready():
 	pass # Replace with function body.
 	
@@ -10,6 +12,10 @@ func _process(delta):
 	if (m_Portals.size() > 0):
 		material.set_shader_param("portal_position", m_Portals[0])
 		material.set_shader_param("portal_direction", m_PortalDirections[0])
+		
+		m_PortalDirections[0] = Vector2(cos(m_TotalDelta), sin(m_TotalDelta))
+		
+	m_TotalDelta += delta
 
 func AddPortal(portal_pos, portal_dir):
 	m_Portals.append(portal_pos)
